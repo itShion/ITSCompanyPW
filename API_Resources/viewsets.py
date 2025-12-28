9905aca15fb3ebc81e43daee43f5c30de6406803
itShion <manalaysaypiero@gmail.com>
2025-12-28 18:29:19 +0100

    feat: Implementazione delle ViewSets

---
+++ API_Resources/viewsets.py
@@ -0,0 +1,25 @@
+﻿from rest_framework import viewsets, permissions
+
+from API_Resources.permissions import IsOwnerOrReadOnly
+from API_Resources.serializers import RisorsaSerializer, TipoRisorsaSerializer
+from Risorsa.models import TipoRisorsa, Risorsa
+
+__all__ = [
+    "RisorsaAPIViewSet",
+]
+
+class RisorsaAPIViewSet(viewsets.ModelViewSet):
+    queryset = Risorsa.objects.all()
+    serializer_class = RisorsaSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class TipoRisorsaAPIViewSet(RisorsaAPIViewSet):
+    queryset = TipoRisorsa.objects.all()
+    serializer_class = TipoRisorsaSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)

a73171b7a47423e85a51d7cc787397c19bd648ec
itShion <manalaysaypiero@gmail.com>
2025-12-30 15:42:24 +0100

    feat: Integrazione di Swagger-UI Per non dover indovinare quali sono gli endpoint ho integrato Swagger

--- API_Resources/viewsets.py
+++ API_Resources/viewsets.py
@@ -1,4 +1,5 @@
-﻿from rest_framework import viewsets, permissions
+﻿from drf_spectacular.utils import extend_schema_view, extend_schema
+from rest_framework import viewsets, permissions
 
 from API_Resources.permissions import IsOwnerOrReadOnly
 from API_Resources.serializers import RisorsaSerializer, TipoRisorsaSerializer
@@ -6,15 +7,32 @@ from Risorsa.models import TipoRisorsa, Risorsa
 
 __all__ = [
     "RisorsaAPIViewSet",
+    "TipoRisorsaAPIViewSet",
 ]
+
+@extend_schema_view(
+    list=extend_schema(tags=['Risorse']),
+    create=extend_schema(tags=['Risorse']),
+    retrieve=extend_schema(tags=['Risorse']),
+    update=extend_schema(tags=['Risorse']),
+    partial_update=extend_schema(tags=['Risorse']),
+    destroy=extend_schema(tags=['Risorse']),
+)
 
 class RisorsaAPIViewSet(viewsets.ModelViewSet):
     queryset = Risorsa.objects.all()
     serializer_class = RisorsaSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-
-class TipoRisorsaAPIViewSet(RisorsaAPIViewSet):
+@extend_schema_view(
+    list=extend_schema(tags=['Tipi Risorsa']),
+    create=extend_schema(tags=['Tipi Risorsa']),
+    retrieve=extend_schema(tags=['Tipi Risorsa']),
+    update=extend_schema(tags=['Tipi Risorsa']),
+    partial_update=extend_schema(tags=['Tipi Risorsa']),
+    destroy=extend_schema(tags=['Tipi Risorsa']),
+)
+class TipoRisorsaAPIViewSet(viewsets.ModelViewSet):
     queryset = TipoRisorsa.objects.all()
     serializer_class = TipoRisorsaSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]

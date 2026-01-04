5cca6b4eb5be72e350e54ea8eeeceea444ad32c4
itShion <manalaysaypiero@gmail.com>
2026-01-04 14:45:01 +0100

    feat: Aggiunta delle classi per il frontend

---
+++ Frontend/FrontendResources/src/models/Risorsa.ts
@@ -0,0 +1,12 @@
+﻿import { TipoRisorsa } from './TipoRisorsa';
+
+export interface Risorsa {
+  id: number;
+  nome: string;
+  descrizione: string;
+  is_available: boolean;
+  capacita: number;
+  tipo : TipoRisorsa;
+}
+
+

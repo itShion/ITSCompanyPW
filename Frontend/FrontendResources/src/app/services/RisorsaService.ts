ceb9b021074cfdafb42783d37415897042e98561
itShion <manalaysaypiero@gmail.com>
2026-01-09 18:11:11 +0100

    feat: Aggiunta del servizio per chiamate API Backend

---
+++ Frontend/FrontendResources/src/app/services/RisorsaService.ts
@@ -0,0 +1,27 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Risorsa } from '../../models/Risorsa';
+import { TipoRisorsa } from '../../models/TipoRisorsa';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RisorsaService {
+  private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:8000/api/v1';
+
+  getRisorse(): Observable<Risorsa[]> {
+    return this.http.get<Risorsa[]>(`${this.apiUrl}/risorse/`);
+  }
+
+  getRisorsa(id: number): Observable<Risorsa> {
+    return this.http.get<Risorsa>(`${this.apiUrl}/risorse/${id}/`);
+  }
+
+  getTipiRisorsa(): Observable<TipoRisorsa[]> {
+    return this.http.get<TipoRisorsa[]>(`${this.apiUrl}/tipo-risorse/`);
+  }
+}

72f041970ccd45697c4765443f6d556a4cf9c45d
Shion <manalaysaypiero@gmail.com>
2026-01-21 16:50:35 +0100

    feature: Duplicated import 'provideHttpClient' in app.config.ts

--- Frontend/FrontendResources/src/app/app.config.ts
+++ Frontend/FrontendResources/src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 

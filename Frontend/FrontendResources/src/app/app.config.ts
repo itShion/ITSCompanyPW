e7bfc6badab6751c7ecde63ff187c74f92ce0e49
itShion <manalaysaypiero@gmail.com>
2026-01-31 22:57:57 +0100

    feat: Prenotazione now is on!

--- Frontend/FrontendResources/src/app/app.config.ts
+++ Frontend/FrontendResources/src/app/app.config.ts
@@ -1,13 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from '../Core/Interceptor/auth.interceptor';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withFetch()),
+        provideHttpClient(
+      withInterceptors([authInterceptor])
+    )
   ]
 };
--- Frontend/FrontendResources/src/prenota/prenota.ts
+++ Frontend/FrontendResources/src/prenota/prenota.ts
@@ -3,18 +3,30 @@ import { ActivatedRoute } from '@angular/router';
 import { RisorsaService } from '../app/services/RisorsaService';
 import { signal } from '@angular/core';
 import { Risorsa } from '../models/Risorsa';
+import { AuthService } from '../app/services/auth.service';
+import { FormsModule } from '@angular/forms';
+import { PrenotazioneDTO } from '../models/Prenotazione';
+import { PrenotaService } from '../app/services/prenota-service';
 
 @Component({
   selector: 'app-prenota',
-  standalone: true,
+  imports: [FormsModule],
   templateUrl: './prenota.html',
   styleUrls: ['./prenota.css'],
 })
 export class Prenota {
   private risorsaService = inject(RisorsaService);
+  private prenotaService = inject(PrenotaService);
   private route = inject(ActivatedRoute);
 
+  private authService = inject(AuthService);
+
+  user = this.authService.getCurrentUser();
+
   risorsa = signal<Risorsa | null>(null);
+
+  dataInizio : string = "";
+  dataFine : string = "";
 
   constructor() {
     const id = Number(this.route.snapshot.paramMap.get('id'));
@@ -25,6 +37,26 @@ export class Prenota {
     this.risorsaService.getRisorsa(id).subscribe({
       next: (data) => this.risorsa.set(data), 
       error: (err) => console.error('Errore:', err),
+    });
+  }
+
+prenota() {
+    const risorsaVal = this.risorsa();
+    if (!risorsaVal || !this.dataInizio || !this.dataFine) {
+      console.error('Dati mancanti');
+      return;
+    }
+
+    const dto: PrenotazioneDTO = {
+      risorsa: risorsaVal.id,
+      data_inizio: this.dataInizio,
+      data_fine: this.dataFine,
+      stato: 'TRUE'
+    };
+
+    this.prenotaService.createPrenotazione(dto).subscribe({
+      next: (res) => console.log('Prenotazione creata', res),
+      error: (err) => console.error(err)
     });
   }
 }

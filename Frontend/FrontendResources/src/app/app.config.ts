6cc5a13d6dfe3738b565c6d35e249a2435008ed2
itShion <manalaysaypiero@gmail.com>
2026-02-01 22:14:40 +0100

    fix: Fixato il problema del white screen al reload delle pagine

--- Frontend/FrontendResources/src/app/app.config.ts
+++ Frontend/FrontendResources/src/app/app.config.ts
@@ -1,16 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from '../Core/Interceptor/auth.interceptor';
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-        provideHttpClient(
-      withInterceptors([authInterceptor])
-    )
-  ]
+providers: [
+  provideBrowserGlobalErrorListeners(),
+  provideRouter(routes, withHashLocation()),
+  provideHttpClient(
+    withInterceptors([authInterceptor])
+  )
+]
 };

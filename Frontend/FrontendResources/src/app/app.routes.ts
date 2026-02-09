3295b09926f68db903b59823b267033612bd318b
Alessandro Asebias <a.asebias@netribegroup.com>
2026-02-09 22:00:48 +0100

    feat: aggiunto login page html e css puro e creazione component

--- Frontend/FrontendResources/src/app/app.routes.ts
+++ Frontend/FrontendResources/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { RisorseListComponent } from '../risorse-list/risorse-list';
 import { Login } from '../login/login';
 import { RegisterComponent } from '../register/register';
 import { Prenota } from '../prenota/prenota';
+import { Loginv2 } from '../loginv2/loginv2';
 
 export const routes: Routes = [
   { path: 'prenotazionitab', component: Prenotazionitab },
@@ -12,4 +13,5 @@ export const routes: Routes = [
   { path: 'login', component: Login},
   { path: 'register', component: RegisterComponent},
   {path: 'prenota/:id', component:Prenota},
+  {path: 'loginv2', component:Loginv2},
 ];
---
+++ Frontend/FrontendResources/src/loginv2/loginv2.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loginv2',
+  imports: [],
+  templateUrl: './loginv2.html',
+  styleUrl: './loginv2.css',
+})
+export class Loginv2 {
+
+}

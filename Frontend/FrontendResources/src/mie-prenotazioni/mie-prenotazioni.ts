4a4801af1ca447dc956d34ba17fce1f8a26e9199
ilamala04 <ilariamala04@gmail.com>
2026-02-12 23:12:29 +0100

    aggiunta component samuele pagina mie prenotazioni

---
+++ Frontend/FrontendResources/src/mie-prenotazioni/mie-prenotazioni.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mie-prenotazioni',
+  imports: [],
+  templateUrl: './mie-prenotazioni.html',
+  styleUrl: './mie-prenotazioni.css',
+})
+export class MiePrenotazioni {
+
+}

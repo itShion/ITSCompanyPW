4e035c847d919be7fb1ec04efa65b7b0f2a66b43
Alessandro Asebias <acasoc53@gmail.com>
2026-02-17 12:32:31 +0100

    feat: modificato collegamento con loginv2

---
+++ Frontend/FrontendResources/src/app/registerv2/registerv2.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registerv2',
+  imports: [],
+  templateUrl: './registerv2.html',
+  styleUrl: './registerv2.css',
+})
+export class Registerv2 {
+
+}

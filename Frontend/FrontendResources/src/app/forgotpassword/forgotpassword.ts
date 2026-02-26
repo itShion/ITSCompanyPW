0b4cdd8ee30f772cc3fa20b52d3cf726469c9012
Alessandro Asebias <acasoc53@gmail.com>
2026-02-26 16:34:49 +0100

    feat/forgot_password: aggiunto pagina di forgot password, non ancora funzionante, e anche immagine dell'ufficio

--- Frontend/FrontendResources/src/app/forgotpassword/forgotpassword.ts
+++ Frontend/FrontendResources/src/app/forgotpassword/forgotpassword.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-forgotpassword',
-  imports: [],
+  selector: 'app-forgot-password',
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './forgotpassword.html',
   styleUrl: './forgotpassword.css',
+  encapsulation: ViewEncapsulation.None
 })
-export class Forgotpassword {
-
+export class ForgotPassword {
+  // Per ora vuoto, aggiungeremo la logica dopo
 }

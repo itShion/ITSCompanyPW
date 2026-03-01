49a7683fc92a223cb58f4a0bf490af11f4afa2c9
Aymanovick <husseiniayman13@gmail.com>
2026-03-01 21:10:34 +0100

    feature(roles): Aggiunta della gestione dei ruoli nel modello di Utente

--- CompanyResources/Utente/models.py
+++ CompanyResources/Utente/models.py
@@ -4,9 +4,28 @@ from django.db import models
 # Create your models here.
 
 class Utente(models.Model):
+
+    RUOLI = [
+        ('UTENTE', 'utente'),
+        ('RESPONSABILE', 'responsabile'),
+        ('ADMIN', 'amministratore'),
+    ]
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     ruolo = models.CharField(max_length=120)
     telefono = models.CharField(max_length=10)
 
     class Meta:
         db_table = 'Utente'
+
+    def __str__(self):
+        return f"{self.user.username} - {self.get_ruolo_display()}"
+
+    def is_utente(self):
+        return self.ruolo == 'UTENTE'
+
+    def is_responsabile(self):
+        return self.ruolo == 'RESPONSABILE'
+
+    def is_admin(self):
+        return self.ruolo == 'ADMIN' or self.user.is_superuser
